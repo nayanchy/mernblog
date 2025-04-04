33d773cd0dec1e96e8583d7a474b85ec77fd3cbb
Nayan Chowdhury <nayan.1aacl@gmail.com>
2025-04-04 21:53:09 +0600

    Clerk Auth integrated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,63 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/pages/About.jsx";
+import HomePage from "./components/pages/HomePage.jsx";
+import SinglePostPage from "./components/pages/SinglePostPage.jsx";
+import LoginPage from "./components/pages/LoginPage.jsx";
+import RegisterPage from "./components/pages/RegisterPage.jsx";
+import Write from "./components/pages/Write.jsx";
+import PostListPage from "./components/pages/PostListPage.jsx";
+import MainLayout from "./components/Layouts/MainLayout.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-createRoot(document.getElementById('root')).render(
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/posts",
+        element: <PostListPage />,
+      },
+      {
+        path: "/:slug",
+        element: <SinglePostPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </StrictMode>
+);
